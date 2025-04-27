9383566daa8f1936d6cab608d80b4881bc6feec4
candelaojeda <ojedacandelaco@gmail.com>
2025-04-27 08:55:36 -0300

    added new home products component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import HeaderSlider from "@/components/HeaderSlider";
+import HomeProducts from "@/components/HomeProducts";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
+        <HomeProducts />
       </div>
     </>
   );
