ddb35d97d0a711c4f06847d8ad669d7b24ed2e24
candelaojeda <ojedacandelaco@gmail.com>
2025-04-27 08:55:17 -0300

    added new products dummy

--- assets/assets.js
+++ assets/assets.js
@@ -4,6 +4,9 @@ import user_icon from "./user_icon.svg";
 import digital_camera from "./digital-technology-camera.png";
 import computer_156_pulgadas_i3 from "./computer-15.6-pulgadas-i3.png";
 import tv_apple from "./tv-apple.png";
+import heart_icon from "./heart_icon.svg";
+import symbol_star from "./symbol_star.svg";
+import symbol_star_light from "./symbol_star_light.svg";
 
 export const assets = {
   logo,
@@ -12,30 +15,143 @@ export const assets = {
   digital_camera,
   computer_156_pulgadas_i3,
   tv_apple,
+  heart_icon,
+  symbol_star,
+  symbol_star_light,
 };
 
 export const productsDummyData = [
   {
-    id: "67a1f4e43f34a77b6dde9144",
+    _id: "67a1f5663f34a77b6dde914c",
     userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
-    name: "Industrial Scanner",
-    description: "Description Name.",
+    name: "Apple Iphone 15 128gb 6.1'",
+    description:
+      "Discover the latest iPhone 15 lineup featuring advanced cameras, sleek designs, and powerful performance.",
     price: 499.99,
     offerPrice: 399.99,
-    image: ["/public/images/industrial-scanner.png"],
-    category: "Category Name",
+    image: [
+      "/images/iphone-15-camera.png",
+      "/images/iphone 15 plus.png",
+      "/images/iphone 15 simple.png",
+    ],
+    category: "Technology",
+    date: 1738667236865,
+    __v: 0,
+  },
+  {
+    _id: "67a1f4e43f34a77b6dde9144",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "Heavy-Duty Industrial Scanner",
+    description:
+      "Efficient and reliable industrial-grade scanner designed for high-volume operations.",
+    price: 419.99,
+    offerPrice: 319.99,
+    image: ["/images/industrial-scanner.png"],
+    category: "Technology",
     date: 1738667236865,
     __v: 0,
   },
   {
     _id: "67a1f52e3f34a77b6dde914a",
     userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
-    name: "Apple Computer",
-    description: "Description Name.",
-    price: 429.99,
-    offerPrice: 329.99,
-    image: ["/public/images/apple-computer.png"],
-    category: "Category Name",
+    name: "Compact Deep Freezer",
+    description:
+      "Keep your goods perfectly frozen with this energy-efficient, compact deep freezer.",
+    price: 829.99,
+    offerPrice: 729.99,
+    image: ["/images/freezer.png"],
+    category: "Electrodomestic",
+    date: 1738667310300,
+    __v: 0,
+  },
+  {
+    _id: "67a1f5993f34a77b6dde914e",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "Educational Puzzle Toy",
+    description:
+      "Engage children's creativity and problem-solving skills with this colorful puzzle toy.",
+    price: 599.99,
+    offerPrice: 399.99,
+    image: ["/images/puzzle-toy.png"],
+    category: "Toys",
+    date: 1738667310300,
+    __v: 0,
+  },
+  {
+    _id: "67a1f5ef3f34a77b6dde9150",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "Standard Office Scanner",
+    description:
+      "Perfect for everyday document scanning needs, combining speed and high-quality output.",
+    price: 1499.99,
+    offerPrice: 1399.99,
+    image: ["/images/scanner-normal.png"],
+    category: "Technology",
+    date: 1738667236865,
+    __v: 0,
+  },
+  {
+    _id: "67a1f70c3f34a77b6dde9156",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "Children’s Train Toy",
+    description:
+      "Delight kids with this vibrant and durable train toy, perfect for imaginative play.",
+    price: 259.99,
+    offerPrice: 159.99,
+    image: ["/images/toy-child-treno.png"],
+    category: "Toys",
+    date: 1738667310300,
+    __v: 0,
+  },
+  {
+    _id: "67a1f7c93f34a77b6dde915a",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "Colorful Kids' Toy Set",
+    description:
+      "A fun and safe toy set designed to spark creativity and joy in young children.",
+    price: 99.99,
+    offerPrice: 89.99,
+    image: ["/images/toy-childrens.png"],
+    category: "Toys",
+    date: 1738667310300,
+    __v: 0,
+  },
+  {
+    _id: "67a1f8363f34a77b6dde915c",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "Mini Train for Kids",
+    description:
+      "Brightly colored mini train that encourages imaginative adventures for children.",
+    price: 99.99,
+    offerPrice: 89.99,
+    image: ["/images/treno-infantil.png"],
+    category: "Toys",
+    date: 1738667310300,
+    __v: 0,
+  },
+  {
+    _id: "67a1f85e3f34a77b6dde915e",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "42-Inch Smart TV",
+    description:
+      "Compact and smart 15-inch TV, perfect for bedrooms, kitchens, or small spaces.",
+    price: 759.99,
+    offerPrice: 659.99,
+    image: ["/images/tv-15-pulgadas-smart.png"],
+    category: "Technology",
+    date: 1738667310300,
+    __v: 0,
+  },
+  {
+    _id: "67a1fa4b3f34a77b6dde9166",
+    userId: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+    name: "55-Inch Smart TV - Black",
+    description:
+      "Experience stunning visuals and smart features with this elegant 55-inch black Smart TV.",
+    price: 1279.99,
+    offerPrice: 1129.99,
+    image: ["/images/tv-55-pulgadas-smart-black.png"],
+    category: "Technology",
     date: 1738667310300,
     __v: 0,
   },
@@ -45,7 +161,7 @@ export const userDummyData = {
   _id: "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
   name: "Candela Ojeda",
   email: "admin@example.com",
-  imageUrl: "/public/images/user-admin 1.png",
+  imageUrl: "/images/user-admin 1.png",
   cartItems: {
     // "67a1f4e43f34a77b6dde9144": 3
   },
