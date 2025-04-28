c2717b90c65a3580a3ffe30d9c68ff7f50173efd
candelaojeda <ojedacandelaco@gmail.com>
2025-04-28 10:20:54 -0300

    import new components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import Navbar from "@/components/Navbar";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
+import FeaturedProduct from "@/components/FeaturedProduct";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
         <HomeProducts />
+        <FeaturedProduct />
+        <Banner />
       </div>
     </>
   );
